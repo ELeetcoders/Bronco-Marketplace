2d57fbd0c30a84ad0004f2cba0f2875b06439198
Michael Truong <truong.minh.michael@gmail.com>
2023-02-16 10:58:04 -0800

    Added spring project for backend

---
+++ api/src/test/kotlin/com/eleetcoders/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eleetcoders.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
