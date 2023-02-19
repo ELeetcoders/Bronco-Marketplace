d16d73eda384a8ed9387003d79dbd181960c4de9
Michael Truong <truong.minh.michael@gmail.com>
2023-02-19 15:20:48 -0800

    Adding 3rd party Kotlin library, Coroutines, with Gradle and implementing that library in a new API endpoint

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- api/src/main/kotlin/com/eleetcoders/api/BaseController.kt
+++ api/src/main/kotlin/com/eleetcoders/api/BaseController.kt
@@ -1,8 +1,10 @@
 package com.eleetcoders.api
 
+import kotlinx.coroutines.delay
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlinx.coroutines.runBlocking
 
 @RestController
 @RequestMapping("/")
@@ -13,5 +15,21 @@ class BaseController {
 
     @GetMapping("/michael")
     fun michael(): String = "API endpoint made by Michael!"
+
+    @GetMapping("/async")
+    fun async(): String {
+        var result = ""
+        /* This can be useful when you do something async, example: call async func */
+        runBlocking {
+            result = doSomethingAsync()
+        }
+        return result
+    }
+
+    suspend fun doSomethingAsync(): String {
+        /* This would be the method that contains an async process, example: calling an api waiting for response */
+        delay(2000)
+        return "Async operation complete after 2 seconds!"
+    }
 
 }
