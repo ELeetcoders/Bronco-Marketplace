10ce5b81cbd33681e39e9f459bf915b2d8e6efe5
Tyler Nijmeh <tylernij@gmail.com>
2023-02-19 13:56:25 -0800

    api: Add demo timestamp endpoint
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- api/src/main/kotlin/com/eleetcoders/api/BaseController.kt
+++ api/src/main/kotlin/com/eleetcoders/api/BaseController.kt
@@ -3,6 +3,9 @@ package com.eleetcoders.api
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 @RestController
 @RequestMapping("/")
@@ -14,4 +17,9 @@ class BaseController {
     @GetMapping("/michael")
     fun michael(): String = "API endpoint made by Michael!"
 
+    @GetMapping("/tyler")
+    fun tyler(): String {
+        val local = LocalDateTime.now();
+        return "The current timestamp is: $local"
+    }
 }
