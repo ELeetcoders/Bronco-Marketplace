ecf0fa6dad21a77c4295305aaa12eae46126ebae
Michael Truong <truong.minh.michael@gmail.com>
2023-02-28 00:23:52 -0800

    Partially fixed service where it returns map in form of '=' instead of ':'

--- api/src/main/kotlin/com/eleetcoders/api/controllers/ProductController.kt
+++ api/src/main/kotlin/com/eleetcoders/api/controllers/ProductController.kt
@@ -1,13 +1,11 @@
 package com.eleetcoders.api.controllers
 
-import com.eleetcoders.api.Product
 import com.eleetcoders.api.services.ProductService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -47,8 +45,9 @@ class ProductController @Autowired constructor(
         return productService.filterByPrice(price)
     }
 
-    @GetMapping("/search")
-    fun searchProduct(@RequestBody term: String) : String? {
-        return productService.searchByTerm(term)
+    data class SearchRequest(val term: String)
+    @PostMapping("/search")
+    fun searchProduct(@RequestBody request: SearchRequest) : String? {
+        return productService.searchByTerm(request.term)
     }
 }
