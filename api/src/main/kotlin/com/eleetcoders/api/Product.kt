4f1805b175261c5c04749355c6bccf5ad5ad73ff
ItzUzi <ugaeta@cpp.edu>
2023-02-28 01:30:35 -0800

    Add Product object

--- api/src/main/kotlin/com/eleetcoders/api/Product.kt
+++ api/src/main/kotlin/com/eleetcoders/api/Product.kt
@@ -1,4 +1,16 @@
 package com.eleetcoders.api
 
-class Product {
+class Product (id:String = "", name:String = "", price:Double = 0.0){
+    val id : String
+    val name : String
+    val price: Double
+
+    init{
+        this.id = id
+        this.name = name
+        this.price = price
+    }
+    override fun toString(): String {
+        return "id: $id\nname: $name\nprice: $price"
+    }
 }
