bb62feaccb780dca7fda124871c5744e23d3bd33
ItzUzi <ugaeta@cpp.edu>
2023-03-18 13:01:24 -0700

    Created deserializer, category enum is now case-insensitive

--- api/src/main/kotlin/com/eleetcoders/api/models/Product.kt
+++ api/src/main/kotlin/com/eleetcoders/api/models/Product.kt
@@ -2,7 +2,12 @@ package com.eleetcoders.api.models
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.google.gson.Gson
+import java.util.*
 
 class Product @JsonCreator constructor(
     @JsonProperty("id") val id: String = "",
@@ -10,7 +15,10 @@ class Product @JsonCreator constructor(
     @JsonProperty("price") val price: Double = -1.0,
     @JsonProperty("email") val email: String = "",
     @JsonProperty("desc") val desc: String = "",
-    @JsonProperty("category") val category: Category = Category.NONE
+
+    @JsonProperty("category", required = true)
+    @JsonDeserialize(using = CategoryDeserializer::class)
+    val category : Category
     ) {
     override fun toString(): String {
         return Gson().toJson(this)
@@ -22,9 +30,11 @@ class Product @JsonCreator constructor(
         TECH,
         SERVICES
     }
-    companion object {
-        fun ignoreCase(input: String): Category {
-            return Category.values().firstOrNull { it.name.equals(input, true) } ?: return Category.NONE
+    class CategoryDeserializer : JsonDeserializer<Category>() {
+        override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): Category {
+            val value = parser.valueAsString.uppercase(Locale.getDefault())
+            return Category.valueOf(value)
         }
+
     }
 }
