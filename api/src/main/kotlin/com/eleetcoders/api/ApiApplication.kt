34ca7d31bbea4098739e36b9ab44754838106c1a
Michael Truong <truong.minh.michael@gmail.com>
2023-04-11 10:29:10 -0700

    trying to merge

--- api/src/main/kotlin/com/eleetcoders/api/ApiApplication.kt
+++ api/src/main/kotlin/com/eleetcoders/api/ApiApplication.kt
@@ -1,11 +1,7 @@
 package com.eleetcoders.api
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
 
 @SpringBootApplication
 class ApiApplication {
