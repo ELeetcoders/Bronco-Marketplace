7bea825e912ddddb9e1605f35ee8dcd5500c464b
Tyler Nijmeh <tylernij@gmail.com>
2023-04-25 09:10:09 -0700

    CorsConfig: Add more localhost origins
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- api/src/main/kotlin/com/eleetcoders/api/CorsConfig.kt
+++ api/src/main/kotlin/com/eleetcoders/api/CorsConfig.kt
@@ -17,6 +17,8 @@ class CorsConfiguration {
         config.addAllowedOrigin("http://broncomarketplace.com")
         config.addAllowedOrigin("https://broncomarketplace.com")
         config.addAllowedOrigin("http://localhost:4200")
+        config.addAllowedOrigin("http://0.0.0.0:4200")
+        config.addAllowedOrigin("http://127.0.0.1:4200")
         config.addAllowedHeader("*")
         config.allowCredentials = true;
         config.addAllowedMethod("OPTIONS")
