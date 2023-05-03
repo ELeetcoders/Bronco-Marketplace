56e548d83267aceceed6b61581c0da4d941fc3fe
Michael Truong <truong.minh.michael@gmail.com>
2023-05-02 23:50:33 -0700

    testing

--- api/src/main/kotlin/com/eleetcoders/api/controllers/LoginController.kt
+++ api/src/main/kotlin/com/eleetcoders/api/controllers/LoginController.kt
@@ -83,8 +83,9 @@ class LoginController constructor(val loginServices: LoginServices){
 
 //        val session = request.getSession(true)
         response.status = HttpStatus.OK.value()
+        return Gson().toJson(Status.VERIFY)
 //        response.setHeader("Set-Cookie", "JSESSIONID=" + session.getId() + "; Path=/; Domain=.broncomarketplace.com; SameSite=Lax; Max-Age=86400");
-        return loginServices.verify(user.email)
+        //return loginServices.verify(user.email)
     }
 
     @PostMapping("sign-out")
